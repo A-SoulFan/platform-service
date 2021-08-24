19b378ae8ea1ee816feac8352658e8b324d727e1
pea <1252464861@qq.com>
2021-08-24 21:07:40 +0800

    common扫描

--- asfbbs-admin/src/main/java/com/asoulfan/asfbbs/admin/AsfbbsAdminApplication.java
+++ asfbbs-admin/src/main/java/com/asoulfan/asfbbs/admin/AsfbbsAdminApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.asoulfan.asfbbs")
 public class AsfbbsAdminApplication {
 
     public static void main(String[] args) {
