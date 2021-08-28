9f67e1cefa8e973af0564f0f2dbe5478adb2b464
antis <934108281@qq.com>
2021-08-28 15:48:42 +0800

    <update>:关闭gateway自动路由注册，开启gateway路由查询接口，增加白名单动态刷新

--- asfbbs-gateway/src/main/java/com/asoulfan/asfbbs/config/IgnoreUrlsConfig.java
+++ asfbbs-gateway/src/main/java/com/asoulfan/asfbbs/config/IgnoreUrlsConfig.java
@@ -3,6 +3,7 @@ package com.asoulfan.asfbbs.config;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -17,6 +18,7 @@ import java.util.List;
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Component
+@RefreshScope
 @ConfigurationProperties(prefix="secure.ignore")
 public class IgnoreUrlsConfig {
     private List<String> urls;
